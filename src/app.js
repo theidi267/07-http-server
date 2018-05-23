e0df53a848c9771f0f99a74107a40da2a6846440
theidi267 <timea.heidenreich@gmail.com>
2018-05-23 07:21:06 -0700

    this is getting out of hand

--- src/app.js
+++ src/app.js
@@ -22,21 +22,20 @@ const requestHandler = (req,res) => {
   // The "if" statements below do the same things that express does (below comments) but 100% manually
     // app.get('/', (req,res) => res.send('Hello From the Gutter'));
     // app.get('/foo/bar/baz', (req,res) => res.send('Hello From the Gutter'));
-  if ( req.method === 'GET' ) {
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 200;
-    res.statusMessage = 'OK';
-
-    // Send out some random HTML (actually, it's not totally random. Note how it includes req.url.query.you ...
-    // That would show whatever you have in the URL after you = (http://localhost:3000?this=that&you=cool
-    res.write(`<!DOCTYPE html><html><body><h1>${req.url.query.you}</h1></body></html>`);
-    // ... Instead of doing manual HTML like that, you could have used the "fs" module to read a file
-    // and "res.write()" the contents of that file.
-
-
-    res.end();
-    return;
-  }
+    if ( req.method === 'GET' ) {
+      fs.readfile('index.html', (err, data) => { 
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 200;
+        res.statusMessage = 'OK';
+        // Send out some random HTML (actually, it's not totally random. Note how it includes req.url.query.you ...
+        // That would show whatever you have in the URL after you = (http://localhost:3000?this=that&you=cool
+        res.write(data.tostring());
+        // ... Instead of doing manual HTML like that, you could have used the "fs" module to read a file
+        // and "res.write()" the contents of that file.
+        res.end();
+        return;
+      })
+    }
 
   // Here, we have a "POST" request which will always return a JSON object.  That object will either be
   // the JSON that you posted in (just spitting it back out), or an error object, formatted to look like JSON
