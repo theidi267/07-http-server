4ab2a3191e2793b3aafdb2fbd21f6af101e2f60b
theidi267 <timea.heidenreich@gmail.com>
2018-06-02 11:13:31 -0700

    trying to pass tests

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ const dotenv = require('dotenv').config(); // eslint-disable-line
 
 const server = require('./src/app.js');
 
-const cowsay = requiure('cowsay'); // eslint-disable-line
-
 server.start( process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
--- src/app.js
+++ src/app.js
@@ -8,19 +8,48 @@ const requestParser = require('./lib/parse-request');
 
 const bodyParser = require('./lib/parse-body');
 
+const cowsay = require('cowsay');
+
 const requestHandler = (req,res) => {
 
   requestParser.execute(req);
 
-  if ( req.method === 'GET' && req.url.pathname === '/') {
-    fs.readFile('index.html', (err, data) => { 
+  if (req.method === 'GET' && req.url.pathname === '/') {
+    fs.readFile('./index.html', (err, data) => {
       if (err) throw err;
 
       let result = data.toString().replace(/Change Content/, req.url.query.that);
 
       res.setHeader('Content-Type', 'text/html');
       res.statusCode = 200;
-      res.statusMessage = 'OK';
+      res.statusMessage = 'Ok';
+      res.write(result);
+      res.end();
+      return;
+    });
+  }
+
+  if ( req.method === 'GET' && req.url.pathname === '/cowsay') {
+    fs.readFile('./index.html', (err, data) => {
+      if (err) throw err;
+
+      let cowtalk = null;
+
+      if (req.url.query.text) {
+        cowtalk = cowsay.say({
+          text: req.url.query.text,
+        });
+      } else {
+        cowtalk = cowsay.say({
+          text: 'meow',
+        });
+      }
+
+      let result = data.toString().replace(/Cowsay Content/, cowtalk);
+
+      res.setHeader('Content-Type', 'text/html');
+      res.statusCode = 200;
+      res.statusMessage = 'Ok';
       res.write(result);
       res.end();
       return;
