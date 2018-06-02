5a11e4eb4c0ccb1ab0f46d613c86fd47397136bb
theidi267 <timea.heidenreich@gmail.com>
2018-06-02 10:41:56 -0700

    cowsay works

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ const dotenv = require('dotenv').config(); // eslint-disable-line
 
 const server = require('./src/app.js');
 
+const cowsay = requiure('cowsay'); // eslint-disable-line
+
 server.start( process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
