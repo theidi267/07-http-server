e594b077541162f5ec9f7874905d1a265402d84c
theidi267 <timea.heidenreich@gmail.com>
2018-06-02 10:34:08 -0700

    linter fixed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config(); // eslint-disable-line
 
 const server = require('./src/app.js');
 
-server.start( process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`))
+server.start( process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
